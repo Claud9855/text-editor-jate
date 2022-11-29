2661528385e81d10b0dc7c75a8eb337ac4ce5c9b
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-11-28 20:00:45 -0500

    fixed loading and saving functionality for IndexDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({jate: content});
+  const request = store.put({id: 1, value: content});
 
   const result = await request;
 };
@@ -30,7 +30,7 @@ export const getDb = async () => {
   const request = store.getAll();
 
   const result = await request;
-  
+  return result?.value;
 };
 
 initdb();
